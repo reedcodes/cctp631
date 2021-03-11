4d20baefd760d615f9c9f5b862127f39846e40b0
Reed Piernock <reed.piernock@georgetown.edu>
2021-03-11 14:57:50 -0500

    adjust styles for video embed

--- src/js/video.js
+++ src/js/video.js
@@ -1,7 +1,10 @@
 $(document).ready( () => {
 
+  // Grab the video embed container.
+  const embed = $( '.video-embed' );
+
   // Find the ID for the video.
-  const videoId = $( '.video' ).attr( 'data-id' );
+  const videoId = embed.attr( 'data-id' );
   const videoSrc = 'https://www.youtube.com/embed/' + videoId + '?autoplay=1';
 
   // Find the button to start the video.
@@ -10,12 +13,13 @@ $(document).ready( () => {
   // Set up the YouTube iframe.
   const video = $( document.createElement( 'iframe' ) );
   video.attr( 'src', videoSrc )
-       .attr( 'allow', 'autoplay' );
+       .attr( 'allow', 'autoplay' )
+       .addClass( 'video' );
 
   play.on( 'click', ( e ) => {
 
     play.remove();
-    $( '.video' ).append( video );
+    embed.append( video );
 
   });
 });
